7a08eb6271c8895d66087c31426d86a66d9e3cad
Farid Guzman <ghostrider_x101@msn.com>
2023-11-21 13:57:05 +0100

    added laravel breeze, planetscale/laravel

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,40 +1,22 @@
+import defaultTheme from 'tailwindcss/defaultTheme';
+import forms from '@tailwindcss/forms';
+
 /** @type {import('tailwindcss').Config} */
 export default {
-    purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
     content: [
-        "./index.html",
-        "./src/**/*.{js,ts,jsx,tsx,vue}",
+        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+        './storage/framework/views/*.php',
+        './resources/views/**/*.blade.php',
+        './resources/js/**/*.vue',
     ],
-    theme: {
-        fontFamily: {
-            'sans': [
-                '"PT Mono", sans-serif',
-            ],
-        },
-        'display': ['PT Mono'],
-        'body': ['PT Mono'],
-        extend: {},
-    },
-    plugins: [
-        require("daisyui"),
-        require("@tailwindcss/typography")
-    ],
-    daisyui: {
-        themes: [
-            {
-                mytheme: {
-                    "primary": "#7700ff",
-                    "secondary": "#006dff",
-                    "accent": "#007cff",
-                    "neutral": "#20231e",
-                    "base-100": "#122f26",
-                    "info": "#00b4db",
-                    "success": "#00b32f",
-                    "warning": "#ca3900",
-                    "error": "#ff8e9e",
-                },
-            },
-        ],
-    },
-}
 
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+            },
+        },
+    },
+
+    plugins: [forms],
+};
