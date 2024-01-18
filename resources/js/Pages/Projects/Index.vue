95496fac6d9bd19cd19fdf0738021339c664fb76
faridguzman91 <farid.guzman91@gmail.com>
2024-01-18 23:27:02 +0100

    added link to storage images

--- resources/js/Pages/Projects/Index.vue
+++ resources/js/Pages/Projects/Index.vue
@@ -36,13 +36,13 @@
                                 class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
                                 <th scope="row"
                                     class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                                    {{ project.id}}
+                                    {{ project.id }}
                                 </th>
                                 <td class="px-6 py-4">
-                                    {{ project.name}}
+                                    {{ project.name }}
                                 </td>
                                 <td class="px-6 py-4">
-                                    <img :src="project.image" />
+                                    <img :src="project.image" class="w-12 h-12 rounded-full"/>
                                 </td>
                                 <td class="px-6 py-4">
                                     Edit/Delete
