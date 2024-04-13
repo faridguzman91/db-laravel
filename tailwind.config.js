e5f49da5b2b538f5c6975cf2c043c0d77861560d
Farid Guzman <ghostrider_x101@msn.com>
2024-04-13 20:34:57 +0200

    added tailwind custom class

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 import defaultTheme from 'tailwindcss/defaultTheme';
 import forms from '@tailwindcss/forms';
+import colors from "tailwindcss/colors"
 
 /** @type {import('tailwindcss').Config} */
 export default {
@@ -11,11 +12,39 @@ export default {
     ],
 
     theme: {
-        extend: {
-            fontFamily: {
-                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+        fontFamily: {
+            primary: "PT Mono",
+            body: "PT Mono"
+        },
+        container: {
+            padding: {
+                DEFAULT: "1rem",
+                lg: "3rem"
             },
         },
+        extend: {
+            colors: {
+                "light-primary": "#FAF1E6",
+                "light-secondary": "#FDF4F6",
+                "light-tail-100": "#A9DED2",
+                "light-tail-500": "#54bab9",
+                "dark-primary": "#050402",
+                "dark-secondary": "#1c1d24",
+                "dark-navy-100": "#07567d",
+                "dark-navy-500": "#292d42",
+                accent: {
+                    DEFAULT: "#AC6B34",
+                    hover: "#925a2b"
+                },
+                paragraph: "#ac6b34",
+                blue: colors.blue,
+                indigo: colors.indigo,
+                green: colors.green,
+                red: colors.red
+
+            }
+
+        }
     },
 
     plugins: [forms],
