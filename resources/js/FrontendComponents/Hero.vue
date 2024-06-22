5873c6a90dc66b642da209157e75ba53829ccfae
faridguzman91 <farid.guzman91@gmail.com>
2024-06-22 19:09:16 +0200

    added buttons

--- resources/js/FrontendComponents/Hero.vue
+++ resources/js/FrontendComponents/Hero.vue
@@ -6,25 +6,39 @@
         <div class="container mx-auto h-full">
             <div class="flex h-full items-center pt-8">
                 <div class="flex flex-1 flex-col items-center lg:items-start">
-                    <p class="text-md mb-[22px] text-lg text-accent">
-                        David Bade
+                    <p class="text-md mb-[22px] font-mono text-lg text-accent">
+                        Contemporary Artist
                     </p>
                     <h1
-                        class="text-4xl font-bold leading-[44px] md:text-5xl md:leading-tight md:tracking-[-2px]"
+                        class="text-4xl font-bold font-mono leading-[44px] md:text-5xl md:leading-tight md:tracking-[-2px]"
                     >
-                        <p
-                            class="max-w-[480px] pb-8 pt-4 text-center text-lg md:pb-12 md:pt-6 lg:text-left"
-                        >
-                            Lorem ipsm
-                        </p>
-
-                        Beeldend Kunstenaar
+                        David Bade
                     </h1>
+                        <div class="flex font-mono flex-row w-full justify-start items-center border border-x-0 m-auto">
  <button
-                            class="hover:bg-light-secondary btn btn-md bg-accent transition-all md:btn-lg mt-6"
+                            class="hover:bg-light-secondary hover:text-light-tail-500 btn btn-md bg-accent transition-all md:btn-lg mt-6"
                         >
-                            button
+                            paintings
                         </button>
+<button
+                            class="hover:bg-light-secondary hover:text-light-tail-500 btn btn-md bg-accent transition-all md:btn-lg mt-6"
+                        >
+                            sculpture
+                        </button>
+<button
+                            class="hover:bg-light-secondary hover:text-light-tail-500 btn btn-md bg-accent transition-all md:btn-lg mt-6"
+                        >
+                            exhibitions
+                        </button>
+
+<button
+                            class="hover:bg-light-secondary hover:text-light-tail-500 btn btn-md bg-accent transition-all md:btn-lg mt-6"
+                        >
+                            projects
+                        </button>
+
+
+     </div>
 
                 </div>
                 <div class="hidden lg:flex flex-1 justify-end items-end h-full">
