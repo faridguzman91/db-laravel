f34f4cc771b555e4934a2f7a4ce3ec73a871ee73
faridguzman91 <farid.guzman91@gmail.com>
2024-06-22 10:49:28 +0200

    added extra header links

--- resources/js/FrontendComponents/Header.vue
+++ resources/js/FrontendComponents/Header.vue
@@ -1,23 +1,32 @@
 <script setup lang="ts">
-
 </script>
 
 <template>
-<div class="navbar bg-base-100">
+<div class="navbar bg-base-50 border-base-content font-mono">
   <div class="flex-1">
     <a class="btn btn-ghost text-xl">David Bade - Home</a>
   </div>
   <div class="flex-none">
     <ul class="menu menu-horizontal px-1">
-      <li><a>Link</a></li>
+      <li><a>Projects</a></li>
+      <li><a>Exhibitions</a></li>
+      <li><a>Archive</a></li>
+      <li><a>Blog</a></li>
+      <li><a>CV</a></li>
+      <li><a>Bio</a></li>
+      <li><a>Contact</a></li>
       <li>
         <details>
           <summary>
-            Parent
+            Timeline
           </summary>
           <ul class="p-2 bg-base-100 rounded-t-none">
-            <li><a>Link 1</a></li>
-            <li><a>Link 2</a></li>
+            <li><a>2020</a></li>
+            <li><a>2019</a></li>
+            <li><a>2018</a></li>
+            <li><a>2017</a></li>
+            <li><a>2016</a></li>
+            <li><a>2015</a></li>
           </ul>
         </details>
       </li>
